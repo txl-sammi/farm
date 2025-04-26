05244dde80483d0e8c261be4c964379ad8fcb4e2
Michael Han <mykalehan@gmail.com>
2025-04-26 11:55:10 +1000

    link to firebase db for farms

--- web-app/src/app/utils/firebase-utils.ts
+++ web-app/src/app/utils/firebase-utils.ts
@@ -1,8 +1,10 @@
 import { db } from '@/app/firebase';
 import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { GeoPoint } from "firebase/firestore";
 
 const COLLECTIONS = {
   USERS: 'users',
+  FARMS: 'farms',
   // Add other collections as needed
 };
 
@@ -16,9 +18,22 @@ export async function loadUsersFromFirestore(userId: string) {
 }
 
 /**
- * Save user tokens to Firestore
+ * Save user farm to Firestore
  */
 export async function saveUserToFirestore(userId: string, email: string, first_name: string, last_name: string) {
   const userRef = doc(db, COLLECTIONS.USERS, userId);
   await setDoc(userRef, { email, first_name, last_name }, { merge: true });
+}
+
+export async function saveFarmToFirestore(
+  userId: string,
+  location: { latitude: number; longitude: number },
+  plants: string[]
+) {
+
+  const farmRef = doc(db, COLLECTIONS.FARMS, userId);
+  await setDoc(farmRef, {
+    location: new GeoPoint(location.latitude, location.longitude),
+    plants: plants,
+  }, { merge: true });
 }
